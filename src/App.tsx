c7d4cc1ad79da93369621e2daa81a91b270bc7a3
hunghg255 <giahunghust@gmail.com>
2024-09-27 22:14:18 +0700

    ci: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [count, setcount] = useState(1);
+
   return (
     <div className="content">
       <h1>Single Page Application</h1>
+
+      <button onClick={() => setcount(count + 1)}>Click me {count}</button>
     </div>
   );
 };
